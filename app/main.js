d424894d9978fe520dfa39145288c422079ed697
Adam Reiter <adr5240@gmail.com>
2017-04-07 19:52:35 -0400

    Wrote first tests

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
-angular.module('AddressBook', []);
+angular.module('AddressBook', [])
+    .service("contactService", function($http) {
+        this.contacts = [];
+        $http.get("http://localhost:9000/contacts", function(res) {
+            console.log(res);
+        });
+    })
+;
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,27 @@
+let assert = chai.assert;
+let expect = chai.expect;
+
 describe("The AddressBook App", function() {
-    it("should exist", function() {
-        chai.assert.isArray([]);
+
+    describe("the contact service", function() {
+
+        beforeEach(function() {
+            module("AddressBook");
+            inject(function($injector) {
+                contactService = $injector.get("contactService");
+                $httpBackend = $injector.get("$httpBackend");
+            });
+        });
+
+        it("should have a property contacts, an array", function() {
+            expect(contactService.contacts).to.be.an('array');
+        });
+
+        it("should call the http backend", function() {
+            $httpBackend.expectGET("http://localhost:9000/contacts")
+                .respond(200, []);
+            $httpBackend.flush();
+        });
     });
+
 });
