fcf259a205b6d4a3c368dc9cee278083566704cc
Adam Reiter <adr5240@gmail.com>
2017-04-07 20:28:58 -0400

    Add Filter/Filter tests

--- app/main.js
+++ app/main.js
@@ -5,13 +5,25 @@ angular.module('AddressBook', [])
 
         $http.get("http://localhost:9000/contacts")
             .then(function(res) {
-                console.log(res);
                 while (res.data[0]) {
                     contactService.contacts.push(res.data.pop());
                 }
         });
     })
+
     .controller("ContactCtrl", function(contactService, $scope) {
         $scope.contacts = contactService.contacts;
+    })
+
+    .filter("proper", function() {
+        return function(name) {
+            let type = typeof(name);
+
+            if (type !== 'number' && type !== 'string') throw new Error();
+
+            return name.toString().split(" ").map(function(word) {
+                return word[0].toUpperCase().concat(word.slice(1));
+            }).join(" ");
+        };
     })
 ;
--- test/main.spec.js
+++ test/main.spec.js
@@ -38,7 +38,30 @@ describe("The AddressBook App", function() {
             $controller("ContactCtrl", { $scope:$scope, contactService:contactService });
             assert.isArray($scope.contacts);
         });
+    });
 
+    describe("the proper filter", function() {
+        beforeEach(function() {
+            module("AddressBook");
+            inject(function($injector) {
+                proper = $injector.get("$filter")("proper");
+            });
+        });
+
+        it("should apply proper case to a string", function() {
+            expect(proper("ned stark")).to.equal("Ned Stark");
+            expect(proper("cersei lannister")).to.equal("Cersei Lannister");
+        });
+
+        it("should be able to take a number, and return a string", function() {
+            expect(proper(42)).to.equal("42");
+        });
+
+        it("should throw an error on incompatible type", function() {
+            assert.throws(function() { proper([]); });
+            assert.throws(function() { proper({}); });
+            assert.throws(function() { proper(undefined); });
+        });
     });
 
 });
